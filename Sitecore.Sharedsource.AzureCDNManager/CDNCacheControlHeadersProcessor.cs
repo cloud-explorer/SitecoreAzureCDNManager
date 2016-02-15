2280f634ccce0733cca2d5c2474b1254b6ce5e06
rrangan <rrangan@gmail.com>
2016-02-15 11:26:06 -0500

    rename CacheControlHeader processor, added MediaProvider and updates TDS project

---
+++ Sitecore.Sharedsource.AzureCDNManager/CDNCacheControlHeadersProcessor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Sitecore.ApplicationCenter.Applications;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.Resources.Media;
+using Sitecore.Web;
+
+namespace Sitecore.Sharedsource.AzureCDNManager
+{
+    public class CDNCacheControlProcessor : HttpRequestProcessor
+    {
+        /// <summary>
+        /// Sets the cache-control header for the requested item
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public override void Process(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, "args");
+            Item item = Context.Item;
+            if (item == null)
+            {
+                //If the context item is null, check if this is a request for a media item on the actual page 
+                MediaRequest request = MediaManager.ParseMediaRequest(args.Context.Request);
+                if (request == null) return;
+                Media media = MediaManager.GetMedia(request.MediaUri);
+                item = media.MediaData.MediaItem;
+            }
+            //Check if this item is based on the caching info inemplate
+            if (!item.InheritsFrom(AzureCDNConstants.AzureCachingInfoTemplateId)) return;
+            //Get the TTL chosed by the editor
+            string s = item["ttl"];
+            int ttl;
+            //If zero or non int value was chosen, use default behaviour 
+            if (!int.TryParse(s, out ttl) || ttl <= 0) return;
+            //Set the response headers
+            args.Context.Response.Cache.SetCacheability(HttpCacheability.Public);
+            TimeSpan delta = TimeSpan.FromMinutes(ttl);
+            args.Context.Response.Cache.SetMaxAge(delta);
+            args.Context.Response.Cache.SetExpires(DateTime.UtcNow + delta);
+            args.Context.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            args.Context.Response.Cache.SetLastModified(item.Statistics.Updated);
+
+        }
+    }
+}
---
+++ Sitecore.Sharedsource.AzureCDNManager/CDNUrlMediaProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Resources.Media;
+
+namespace Sitecore.Sharedsource.AzureCDNManager
+{
+    public class CDNUrlMediaProvider : MediaProvider
+    {
+        public override string GetMediaUrl(MediaItem item, MediaUrlOptions options)
+        {
+            string mediaUrl = base.GetMediaUrl(item, options);
+            if (string.IsNullOrEmpty(mediaUrl)) return mediaUrl;
+
+            int versionNumber = item.InnerItem.Version.Number;
+            string langIsoCode = item.InnerItem.Language.CultureInfo.TwoLetterISOLanguageName;
+            string updatedDate = item.InnerItem.Statistics.Updated.ToString("s");
+            NameValueCollection parameters = new NameValueCollection
+            {
+                {"v", versionNumber.ToString()},
+                {"lang", langIsoCode},
+                {"modified", updatedDate}
+            };
+            if (options.AlwaysIncludeServerUrl)
+            {
+                UriBuilder uriBuilder = new UriBuilder(mediaUrl);
+                NameValueCollection queryString = HttpUtility.ParseQueryString(uriBuilder.Query);
+                queryString.Add(parameters);
+                uriBuilder.Query = ToQueryString(queryString);
+                return uriBuilder.ToString();
+            }
+            StringBuilder sb = new StringBuilder(mediaUrl);
+            string seperator = "?";
+            if (mediaUrl.Contains(seperator)) seperator = "&";
+            foreach (string key in parameters.Keys)
+            {
+                sb.AppendFormat("{0}{1}={2}", seperator, key, parameters[key]);
+                seperator = "&";
+            }
+            return sb.ToString();
+        }
+
+        private string ToQueryString(NameValueCollection nvc)
+        {
+            var array = (from key in nvc.AllKeys
+                         from value in nvc.GetValues(key)
+                         select string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(value)))
+                .ToArray();
+            return "?" + string.Join("&", array);
+        }
+    }
+}
